f6f5cf3e38c186017c746c973bb377919423b76b
gurdian0614 <ivan.gurdian@outlook.com>
2025-07-23 19:24:54 -0600

    Adding data

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Pedidos;
+
+Producto producto = new Producto();
+producto.Id = 1;
+producto.Nombre = "Laptop";
+producto.Precio = 5000;
+producto.Stock = 10;
+
+producto.MostrarInformacion();
+producto.DisminuirStock(2);
+producto.MostrarInformacion();
+producto.DisminuirStock(15);
+producto.MostrarInformacion();
+
+Producto producto1 = new Producto(); 
+producto1.Id = 2;
+producto1.Nombre = "Mouse";
+producto1.Precio = 250;
+producto1.Stock = 15;
+
+producto1.MostrarInformacion();
