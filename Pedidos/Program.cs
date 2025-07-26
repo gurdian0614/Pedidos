f609ad7ef3e044cbe63e15a0fbdc42e3812eaba3
gurdian0614 <ivan.gurdian@outlook.com>
2025-07-25 19:19:56 -0600

    Adding data

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -8,10 +8,6 @@ producto.Precio = 5000;
 producto.Stock = 10;
 
 producto.MostrarInformacion();
-producto.DisminuirStock(2);
-producto.MostrarInformacion();
-producto.DisminuirStock(15);
-producto.MostrarInformacion();
 
 Producto producto1 = new Producto(); 
 producto1.Id = 2;
@@ -19,4 +15,12 @@ producto1.Nombre = "Mouse";
 producto1.Precio = 250;
 producto1.Stock = 15;
 
+producto1.MostrarInformacion();
+
+Pedido pedido = new Pedido(1);
+pedido.AgregarItem(producto, 1);
+pedido.AgregarItem(producto1, 5);
+pedido.MostrarDetalles();
+
+producto.MostrarInformacion();
 producto1.MostrarInformacion();
