dfc2d7aeac6d96f9e82baf079a4e074e84819d88
gurdian0614 <ivan.gurdian@outlook.com>
2025-07-30 18:39:00 -0600

    Adding data

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -34,3 +34,23 @@ electronico.Stock = 200;
 electronico.GarantiaMeses = 12;
 electronico.Voltaje = "110V";
 electronico.MostrarInformacion();
+
+Libro libro = new Libro();
+libro.Id = 4;
+libro.Nombre = "El Señor de los Anillos";
+libro.Precio = 250;
+libro.Stock = 1500;
+libro.ISBN = "9788845292613";
+libro.Autor = "J.R.R. Tolkien";
+libro.NumeroPaginas = 500;
+libro.MostrarInformacion();
+
+Libro libro1 = new Libro();
+libro1.Id = 5;
+libro1.Nombre = "Luna de Plutón";
+libro1.Precio = 250;
+libro1.Stock = 1600;
+libro1.ISBN = "6070731743";
+libro1.Autor = "Dross";
+libro1.NumeroPaginas = 471;
+libro1.MostrarInformacion();
