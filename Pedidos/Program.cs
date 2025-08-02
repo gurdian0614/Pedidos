d9fbdb91724e1233f96063898bfcccf7aa87285c
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-01 19:01:12 -0600

    Adding data

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -2,30 +2,6 @@
 using Pedidos;
 using Pedidos.Herencia;
 
-Producto producto = new Producto();
-producto.Id = 1;
-producto.Nombre = "Laptop";
-producto.Precio = 5000;
-producto.Stock = 10;
-
-producto.MostrarInformacion();
-
-Producto producto1 = new Producto(); 
-producto1.Id = 2;
-producto1.Nombre = "Mouse";
-producto1.Precio = 250;
-producto1.Stock = 15;
-
-producto1.MostrarInformacion();
-
-Pedido pedido = new Pedido(1);
-pedido.AgregarItem(producto, 1);
-pedido.AgregarItem(producto1, 5);
-pedido.MostrarDetalles();
-
-producto.MostrarInformacion();
-producto1.MostrarInformacion();
-
 Electronico electronico = new Electronico();
 electronico.Id = 3;
 electronico.Nombre = "iPhone 17 Pro Max";
@@ -33,6 +9,13 @@ electronico.Precio = 1000;
 electronico.Stock = 200;
 electronico.GarantiaMeses = 12;
 electronico.Voltaje = "110V";
+electronico.MostrarInformacion();
+
+Pedido pedido = new Pedido(1);
+pedido.AgregarItem(electronico, 1);
+pedido.AgregarItem(electronico, 5);
+pedido.MostrarDetalles();
+
 electronico.MostrarInformacion();
 
 Libro libro = new Libro();
@@ -45,6 +28,13 @@ libro.Autor = "J.R.R. Tolkien";
 libro.NumeroPaginas = 500;
 libro.MostrarInformacion();
 
+Pedido pedido1 = new Pedido(2);
+pedido1.AgregarItem(libro, 10);
+pedido1.AgregarItem(libro, 51);
+pedido1.MostrarDetalles();
+
+libro.MostrarInformacion();
+
 Libro libro1 = new Libro();
 libro1.Id = 5;
 libro1.Nombre = "Luna de Plutón";
@@ -53,4 +43,11 @@ libro1.Stock = 1600;
 libro1.ISBN = "6070731743";
 libro1.Autor = "Dross";
 libro1.NumeroPaginas = 471;
+libro1.MostrarInformacion();
+
+Pedido pedido2 = new Pedido(3);
+pedido2.AgregarItem(libro1, 15);
+pedido2.AgregarItem(libro1, 25);
+pedido2.MostrarDetalles();
+
 libro1.MostrarInformacion();
