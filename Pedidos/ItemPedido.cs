ca568befce958de5b25b71390aade8cb9542e667
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-01 19:00:35 -0600

    Comment

--- Pedidos/ItemPedido.cs
+++ Pedidos/ItemPedido.cs
@@ -3,10 +3,13 @@ namespace Pedidos
 {
     public class ItemPedido
     {
+        // Puedo declarar objetos de clases abstractas siempre y cuando no las inicialice
+        // es decir Producto = new Producto();
         public Producto Producto { get; set; }
         public int Cantidad { get; set; }
         public decimal Subtotal => Producto.Precio * Cantidad;
 
+        // En este caso estamos asignando el objeto del parametro Producto a la propiedad abstracta Producto
         public ItemPedido(Producto Producto, int Cantidad) 
         {
             this.Producto = Producto;
