147533dcbbc3ff05f4a1259dfc97e1c0c944d845
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-11 19:13:28 -0600

    Apply throw syntax

--- Pedidos/Pedido.cs
+++ Pedidos/Pedido.cs
@@ -9,7 +9,6 @@ namespace Pedidos
         public DateTime Fecha { get; set; }
         public List<ItemPedido> Items { get; set; }
         public decimal Total => Items.Sum(item => item.Subtotal);
-        private bool TieneStock { get; set; }
 
         public Pedido(int IdPedido)
         {
@@ -21,36 +20,34 @@ namespace Pedidos
         // En este caso estamos asignando el objeto abstracto Producto como parametro
         public void AgregarItem(Producto Producto, int Cantidad)
         {
-            if (Producto.Stock >= Cantidad)
+            if (Producto == null)
             {
-                Items.Add(new ItemPedido(Producto, Cantidad));
-                Producto.DisminuirStock(Cantidad);
-                Console.WriteLine($"El Producto {Producto.Nombre} con cantidad {Cantidad}, se ha agregado al Pedido");
-                Console.WriteLine();
-                TieneStock = true;
-            } else
-            {
-                Console.WriteLine($"No hay suficiente stock de {Producto.Nombre}");
-                Console.WriteLine();
-                TieneStock = false;
+                throw new ArgumentException("El producto no puede ser nulo");
             }
+
+            if (Producto.Stock < Cantidad)
+            {
+                throw new InvalidOperationException($"No hay suficiente stock de {Producto.Nombre}");
+            }
+
+            Items.Add(new ItemPedido(Producto, Cantidad));
+            Producto.DisminuirStock(Cantidad);
+            Console.WriteLine($"El Producto {Producto.Nombre} con cantidad {Cantidad}, se ha agregado al Pedido");
+            Console.WriteLine();
         }
 
         public void MostrarDetalles()
         {
-            if(TieneStock)
-            {
-                Console.WriteLine("-----------------------------------------");
-                Console.WriteLine(ObtenerInformacionDetallada());
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine(ObtenerInformacionDetallada());
 
-                foreach (var item in Items)
-                {
-                    Console.WriteLine($"Producto: {item.Producto.Nombre}, Cantidad: {item.Cantidad}, Subtotal: {item.Subtotal:C}");
-                }
-                Console.WriteLine($"Total: {Total:C}");
-                Console.WriteLine("-----------------------------------------");
-                Console.WriteLine();
+            foreach (var item in Items)
+            {
+                Console.WriteLine($"Producto: {item.Producto.Nombre}, Cantidad: {item.Cantidad}, Subtotal: {item.Subtotal:C}");
             }
+            Console.WriteLine($"Total: {Total:C}");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine();
         }
 
         public string ObtenerInformacionDetallada()
